526c796f1db9af3238729717b08c1df318e880fa
Aditya Bahakar <adityabahakar99@gmail.com>
2022-05-20 21:35:41 +0530

    Player check for option No_play or Laderr or Snake

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,38 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static final int START = 0;
+
+
     public static void main(String[] args){
-        int start = 0;
-        System.out.println("Welcome to Snake And Ladder Game");
-        System.out.println("Start position = "+start);
-        int roll = (int) (Math.floor(Math.random() *10)%6+1);
-        System.out.println("After rolling the dice : "+ roll);
+        int pos = 0;
+        System.out.println("Starting position of player is: " + START);
+        int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Number on rolled dice is : " +roll);
+        int condition = (int) (Math.floor(Math.random() * 10) % 3);
+        System.out.println("Condition: " +condition);
+        switch (condition){
+            case NO_PLAY:
+                System.out.println("New Position is: " + pos);
+                break;
+            case LADDER:
+                pos = pos + roll;
+                System.out.println("New Position is: " + pos);
+                break;
+            case SNAKE:
+                pos = pos - roll;
+                if ( pos >= 0)
+                    System.out.println("New Position is: " + pos);
+                else
+                    pos = 0;
+                System.out.println("New Position is: " + pos);
+                break;
+            default:
+                System.out.println("Default");
+                break;
+        }
     }
-}
+    }
