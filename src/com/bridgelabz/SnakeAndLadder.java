149467a80cecfdbeb64421953c15f7ea56aeffaa
Aditya Bahakar <adityabahakar99@gmail.com>
2022-05-20 21:12:02 +0530

    Player get random no fram 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,5 +5,7 @@ public class SnakeAndLadder {
         int start = 0;
         System.out.println("Welcome to Snake And Ladder Game");
         System.out.println("Start position = "+start);
+        int roll = (int) (Math.floor(Math.random() *10)%6+1);
+        System.out.println("After rolling the dice : "+ roll);
     }
 }
