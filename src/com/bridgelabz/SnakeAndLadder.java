ea0cdf73b134dcf29dfd44c2ef648c8d4bff99a0
Aditya Bahakar <adityabahakar99@gmail.com>
2022-05-20 20:56:26 +0530

    Player initial position added

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+    public static void main(String[] args){
+        int start = 0;
+        System.out.println("Welcome to Snake And Ladder Game");
+        System.out.println("Start position = "+start);
+    }
+}
